0a71e8b94e2b873f9fb6f5e72078cd4e81bf8887
othmanordaski <rouissatothman@gmail.com>
2024-07-19 23:34:46 +0100

    feat(users): add verification token management and user verification
    - Injected `VerificationToken` repository
    - Added `findById` method to locate users by ID
    - Added `findVerificationToken` method to locate verification tokens
    - Added `verifyUser` method to mark users as verified

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { VerificationToken } from './entities/verification-token.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(VerificationToken)
+    private tokenRepository: Repository<VerificationToken>,
   ) {}
 
   async findByEmail(email: string): Promise<User | undefined> {
@@ -20,6 +23,16 @@ export class UsersService {
 
   async create(userData: Partial<User>): Promise<User> {
     const user = this.userRepository.create(userData);
+    return this.userRepository.save(user);
+  }
+  async findById(id: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+  async findVerificationToken(token: string): Promise<VerificationToken> {
+    return this.tokenRepository.findOne({ where: { token } });
+  }
+  async verifyUser(user: User): Promise<User> {
+    user.isVerified = true;
     return this.userRepository.save(user);
   }
 }
