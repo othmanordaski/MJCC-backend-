9940352cd6ae243796883b95ad0911ea56ece612
othmanordaski <rouissatothman@gmail.com>
2024-07-24 21:23:22 +0100

    feat: Add ResetPasswordDto with validation rules
    - Introduced `ResetPasswordDto` class with validation rules for password complexity
    - Ensured password must be at least 14 characters long and include uppercase, lowercase, number, and special character

---
+++ src/reset-password/dto/reset-password.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, IsNotEmpty, MinLength, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ResetPasswordDto {
+  @ApiProperty({
+    description: 'New password',
+    minLength: 14,
+    pattern:
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{14,}$',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{14,}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
+  password: string;
+}
