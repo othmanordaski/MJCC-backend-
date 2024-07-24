396a5495bc300302164bd837588823eec38549a3
othmanordaski <rouissatothman@gmail.com>
2024-07-24 21:19:20 +0100

    feat: Add AuthModule dependency to PasswordResetModule
    - Imported AuthModule in PasswordResetModule
    - Updated imports array to include forwardRef for AuthModule

--- src/reset-password/password-reset.module.ts
+++ src/reset-password/password-reset.module.ts
@@ -5,11 +5,13 @@ import { UsersModule } from 'src/users/users.module';
 import { MailModule } from 'src/mail/mail.module';
 import { RestPasswordController } from './password-reset.controller';
 import { PasswordResetService } from './password-reset.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ForgetToken]),
     forwardRef(() => UsersModule),
+    forwardRef(() => AuthModule),
     MailModule,
   ],
   controllers: [RestPasswordController],
